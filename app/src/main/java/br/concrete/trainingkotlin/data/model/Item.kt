19750c3f32f220e0096a633e9107556cfa769dbf
Alison Soldado <alison.soldado@concrete.com.br>
2018-02-08 16:59:14 -0200

    Adiciona androidExtensions no gradle e parcelize no item

--- app/src/main/java/br/concrete/trainingkotlin/data/model/Item.kt
+++ app/src/main/java/br/concrete/trainingkotlin/data/model/Item.kt
@@ -1,32 +1,10 @@
 package br.concrete.trainingkotlin.data.model
 
-import android.os.Parcel
+import android.annotation.SuppressLint
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@SuppressLint("ParcelCreator")
+@Parcelize
 class Item(val task: String,
-           val description: String) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString())
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(task)
-        parcel.writeString(description)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Item> {
-        override fun createFromParcel(parcel: Parcel): Item {
-            return Item(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Item?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+           val description: String) : Parcelable
